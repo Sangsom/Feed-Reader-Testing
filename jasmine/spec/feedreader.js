c88cacea211d70c0d0d65b270de3baaa8b1ac61d
Rinalds Pudulis <rpudulis@gmail.com>
2018-03-21 14:20:38 +0200

    test: at least 1 entry is loaded to the feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -80,17 +80,27 @@ $(function () {
         });
     });
 
-
-
-
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', () => {
+        const feed = document.querySelector('.feed');
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+        beforeEach(done => {
+            loadFeed(0, () => {
+                done();
+            });
+        });
+
+        /* TODO: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
+        it('at least 1 entry is loaded to the feed', (done) => {
+            expect(feed.childElementCount).toBeGreaterThan(0);
+            done();
+        })
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
